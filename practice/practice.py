821199e7bd0c394d61230e3760feb8e7d5e323ca
이재민 <rkwoals524@hanmail.net>
2023-10-23 08:24:48 +0900

    dictionary problem

--- practice/practice.py
+++ practice/practice.py
@@ -269,14 +269,13 @@ target = 9
 
 output = []
 for i in range(len(nums)):
-    print("i:",i, "nums[i]:", nums[i])
     i_val = nums[i]
     for j in range(1, len(nums)):
         if i == j:
             break
-        print("j:",j, "nums[j]:", nums[j])
         j_val = nums[j]
         if i_val + j_val == target:
             output.append(i_val)
             output.append(j_val)
             print(output)
+
